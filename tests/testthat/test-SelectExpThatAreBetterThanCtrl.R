021c38523d353edb5d699de8b0a0ec5aa339165b
J. Kyle Wathen <Kyle.Wathen@Cytel.com>
2023-12-11 16:13:38 -0600

    Initial addition of code and repository setup

---
+++ tests/testthat/test-SelectExpThatAreBetterThanCtrl.R
@@ -0,0 +1,19 @@
+#################################################################################################### .
+#   Program/Function Name:
+#   Author: Author Name
+#   Description: Test file for SelectExpThatAreBetterThanCtrl
+#   Change History:
+#   Last Modified Date: 12/11/2023
+#################################################################################################### .
+
+
+context( "SelectExpThatAreBetterThanCtrl")
+
+test_that("Test- SelectExpThatAreBetterThanCtrl", {
+    # Example test that will fail
+    # nRet         <- 1
+    # nExpectedRet <- 10
+
+
+    # expect_equal( nRet, nExpectedRet, info = "The test failed...", label ="Test for ..." )
+})
