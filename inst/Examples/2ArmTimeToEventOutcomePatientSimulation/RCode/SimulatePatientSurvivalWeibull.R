14aeb6e220549f780ffdd36201445d99e090a95c
J. Kyle Wathen, PhD <KyleWathen@Gmail.Com>
2024-03-20 14:09:25 -0500

    Updating the R code and the workbook

--- inst/Examples/2ArmTimeToEventOutcomePatientSimulation/RCode/SimulatePatientSurvivalWeibull.R
+++ inst/Examples/2ArmTimeToEventOutcomePatientSimulation/RCode/SimulatePatientSurvivalWeibull.R
@@ -33,15 +33,16 @@ SimulatePatientSurvivalWeibull<- function(NumSub, NumArm, TreatmentID, SurvMetho
     #TODO: Need to test that the paths that hit an error actually stop
     
     # The SurvParam depends on input in East, EAST sends the Median (see the Simulation->Response Generation tab for what is sent)
-    setwd( "C:\\Kyle\\Cytel\\Software\\East-R\\EastRExamples\\Examples\\2ArmTimeToEventOutcomePatientSimulation\\ExampleOutput")
- 
+    # setwd( "C:\\Kyle\\Cytel\\Software\\CyneRgy\\inst\\Examples\\2ArmTimeToEventOutcomePatientSimulation\\ExampleOutput")
+    # saveRDS( UserParam, "UserParams.Rds")
+    # saveRDS( TreatmentID, "TreatmentID.Rds")
     # If you wanted to save the input objects you could use the following to save the files to your working directory
     
     # Example of how to save the data sent from East for each look.
     # If the DesignParam.Rds exists, then don't save it again.
     if( !file.exists(  "DesignParam.Rds" ))
     {
-        saveRDS( SurvParam, paste0( "SurvParam.Rds") )
+        #saveRDS( SurvParam, paste0( "SurvParam.Rds") )
         # Use the same function as previous line if you want to save the other objects
     }
 
@@ -49,44 +50,25 @@ SimulatePatientSurvivalWeibull<- function(NumSub, NumArm, TreatmentID, SurvMetho
     # This will cause SurvParam to be a 2x2 matrix. 
     # For this example we will assume that column 1 are the 2 Weibull parameters for Standard of Care and column 2 are the two Weibull parameters for Experimental 
     
-    vSurvTime <- rep( -1, NumSub )  # The vector of patient survival times that will be returned.  
-    
+    vSurvTime    <- rep( -1, NumSub )  # The vector of patient survival times that will be returned.  
     vTreatmentID <- TreatmentID +1   # If this is 0 then it is control, 1 is treatment. Adding one since vectors are index by 1 
     ErrorCode    <- rep( -1, NumSub ) 
     
+
+    vShapes <- c( UserParam$dShapeCtrl, UserParam$dShapeExp )  
+    vScales <- c( UserParam$dScaleCtrl, UserParam$dScaleExp ) 
     
-    if(SurvMethod == 1)   # Hazard Rates
+    # Simulate the patient survival times based on the treatment
+    # For the Hazard Rate input with 1 piece, this is just simulating from an exponential distribution as an example and results will match
+    # East if you used the build hazard option.
+    for( nPatIndx in 1:NumSub)
     {
-        
-        if( NumPrd == 2 )
-        {
-            vShapes <- SurvParam[ 1, ]   # Row 1 is the shape parameters
-            vScales <- SurvParam[ 2, ]   # Row 2 is the scale parameters
-            # Simulate the patient survival times based on the treatment
-            # For the Hazard Rate input with 1 piece, this is just simulating from an exponential distribution as an example and results will match
-            # East if you used the build hazard option.
-            for( nPatIndx in 1:NumSub)
-            {
-                nPatientTreatment     <- vTreatmentID[ nPatIndx ]
-                vSurvTime[ nPatIndx ] <- rweibull( 1, vShapes[ nPatientTreatment ], vScales[ nPatientTreatment ] )
-                
-            }
-        }
-        else 
-        {
-            ErrorCode <- ERROR1   # CAUSE_AN_ERROR is not defined so should cause in error
-        }
+        nPatientTreatment     <- vTreatmentID[ nPatIndx ]
+        vSurvTime[ nPatIndx ] <- rweibull( 1, vShapes[ nPatientTreatment ], vScales[ nPatientTreatment ] )
         
     }
-    else if(SurvMethod == 2)   # Cumulative % Survivals
-    {
-        ErrorCode <- ERROR1 # ERROR1 is not defined so this will cause an error if the users selects anything besides Hazard Rate and uses this function
         
-    }
-    else if(SurvMethod == 3)   # Median Survival Times
-    {
-        ErrorCode <- ERROR3   # ERROR3 is not defined so this will cause an error if the users selects anything besides Hazard Rate and uses this function
-    }
+
     
     return( list(SurvivalTime = as.double( vSurvTime ), ErrorCode = ErrorCode) )
 }
