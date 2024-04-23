ab071047860ebbe76b4a956711f02bfc52bb5cb3
Shubham Lahoti <166011767+slahoti17@users.noreply.github.com>
2024-04-23 14:40:45 +0530

    Examples: 2-Arm Normal Analysis
    The commit contains:
    
    1. " AnalyzeUsingEastManualFormulaNormal.R " : Example -1 where the computation of 2-Arm Normal test statistic is using Q.3.3 section of East user Manual
    2. " AnalyzeUsingZTestNormal.R ":  Example - 2 where the computation of  2-Arm Normal test statistic is using R package.
    3. " LibraryBSDA.R "  R file includes the function to install library "BSDA" which is required to run Z.test() and can be called using "Initialize R Environment".

---
+++ R/AnalyzeUsingZTestNormal.R
@@ -0,0 +1,89 @@
+#  Last Modified Date: 04/19/2024
+#' @name AnalyzeUsingZTest_Normal
+#' @param SimData Data frame which consists of data generated in current simulation.
+#' @param DesignParam List of Design and Simulation Parameters required to perform analysis.
+#' @param LookInfo List containing Design and Simulation Parameters, which might be required to perform analysis.
+#' @param UserParam A list of user defined parameters in East. The default must be NULL.
+#' @description Use the Z.test() function inside "BSDA" library to compute the statistic.  The purpose of this example is to demonstrate how the analysis and decision making can be modified in a simple approach.  
+#'              The test statistic is compared to the upper boundary computed and sent by East as an input. This example does NOT include a futility rule. 
+#' @return TestStat A double value of the computed test statistic
+#' @return Decision An integer value: Decision = 0 --> No boundary crossed
+#'                                    Decision = 1 --> Lower Efficacy Boundary Crossed
+#'                                    Decision = 2 --> Upper Efficacy Boundary Crossed
+#'                                    Decision = 3 --> Futility Boundary Crossed
+#'                                    Decision = 4 --> Equivalence Boundary Crossed
+#' @return ErrorCode An integer value:  ErrorCode = 0 --> No Error
+#                                       ErrorCode > 0 --> Non fatal error, current simulation is aborted but the next simulations will run
+#                                       ErrorCode < 0 --> Fatal error, no further simulation will be attempted
+
+#'@note Helpful Hints:
+#'       There is often info that East sends to R that are not shown in a given example.  It can be very helpful to save the input 
+#'       objects and then load them into your R session and inspect them.  This can be done with the following R code in your function.
+#'
+#'       saveRDS( SimData,     "SimData.Rds")
+#'       saveRDS( DesignParam, "DesignParam.Rds" )
+#'       saveRDS( LookInfo,    "LookInfo.Rds" )
+#'
+#'       The above code will save each of the input objects to a file so they may be examined within R.
+#' @export
+#' 
+#' 
+#######################################################################################################################################################################################################################
+
+AnalyzeUsingZTestNormal <- function( SimData, DesignParam, LookInfo, UserParam = NULL )
+{   
+       # Input objects can be saved through the following lines:
+    
+    # setwd( "D:\\Project\\CyneRgy\\CyneRgy\\Normal_rds")
+    # saveRDS( SimData, "SimData.Rds")
+    # saveRDS( DesignParam, "DesignParam.Rds" )
+    # saveRDS( LookInfo, "LookInfo.Rds" )
+    
+    
+    # Retrieve necessary information from the objects East sent
+    nLookIndex           <- LookInfo$CurrLookIndex
+    nQtyOfEvents         <- LookInfo$CumEvents[ nLookIndex ]
+    nQtyOfPatsInAnalysis <- LookInfo$CumCompleters[ nLookIndex ]
+    
+    # Create the vector of simulated data for this IA - East sends all of the simulated data
+    vPatientOutcome      <- SimData$Response[ 1:nQtyOfPatsInAnalysis ]
+    vPatientTreatment    <- SimData$TreatmentID[ 1:nQtyOfPatsInAnalysis ]
+    
+    # Create vectors of data for each treatment - E is Experimental and S is Standard of Care 
+    vOutcomesS           <- vPatientOutcome[ vPatientTreatment == 0 ]
+    vOutcomesE           <- vPatientOutcome[ vPatientTreatment == 1 ]
+    
+    # compute the estimates for mean and Std. Dev for (E and S)
+    dMeanOfResponsesOnE   <- mean( vOutcomesE )
+    dStdDevOfResponsesOnE <- sd( vOutcomesE)
+    nQtyOfPatsOnE         <- length( vOutcomesE )
+    
+    dMeanOfResponsesOnS   <- mean( vOutcomesS )
+    dStdDevOfResponsesOnS <- sd( vOutcomesS)
+    nQtyOfPatsOnS         <- length( vOutcomesS )
+    
+
+    # delta = mean(E) - mean(S). Change the alternative if delta < 0, put alternative = "less"
+    lAnalysisResult           <- z.test( vOutcomesE, vOutcomesS, alternative = "greater", sigma.x = dStdDevOfResponsesOnE, 
+                                   sigma.y = dStdDevOfResponsesOnS )
+    
+    dPValue              <- lAnalysisResult$p.value
+    dZValue              <- qnorm( 1 - dPValue )
+    nDecision            <- ifelse( dZValue > LookInfo$EffBdryUpper[ nLookIndex ], 2, 0 )  # A decision of 2 means success, 0 means continue the trial
+    
+    
+    if( nDecision == 0 )
+    {
+        # For this example, there is NO futility check but this is left for consistency with other examples 
+        
+    }
+    
+    Error <-  0
+    
+    
+    lRet <- list( TestStat = as.double( dZValue ),
+                  Decision  = as.integer( nDecision ), 
+                  ErrorCode = as.integer( Error ))
+    return( lRet )
+}
+
