05fcbed4b0bbca1697342fbedfc618d08ff206c8
J. Kyle Wathen, PhD <KyleWathen@Gmail.Com>
2024-04-25 14:38:16 -0500

    Udpate to the Analyze.R function to make using it easier

--- inst/Templates/Analyze.R
+++ inst/Templates/Analyze.R
@@ -4,6 +4,15 @@
 #' @param DesignParam List of Design and Simulation Parameters required to perform analysis.
 #' @param LookInfo List containing Design and Simulation Parameters, which might be required to perform analysis.
 #' @param UserParam A list of user defined parameters in East. The default must be NULL.
+#' @return TestStat A double value of the computed test statistic
+#' @return Decision An integer value: Decision = 0 --> No boundary crossed
+#'                                    Decision = 1 --> Lower Efficacy Boundary Crossed
+#'                                    Decision = 2 --> Upper Efficacy Boundary Crossed
+#'                                    Decision = 3 --> Futility Boundary Crossed
+#'                                    Decision = 4 --> Equivalence Boundary Crossed
+#' @return ErrorCode An integer value:  ErrorCode = 0 --> No Error
+#                                       ErrorCode > 0 --> Non fatal error, current simulation is aborted but the next simulations will run
+#                                       ErrorCode < 0 --> Fatal error, no further simulation will be attempted
 {{FUNCTION_NAME}} <- function(SimData, DesignParam, LookInfo = NULL, UserParam = NULL )
 {
     nError 	        <- 0
