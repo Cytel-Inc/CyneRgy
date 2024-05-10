d98942e93f7b782225fad195e21cd5a0b0766d3c
J. Kyle Wathen, PhD <KyleWathen@Gmail.Com>
2024-05-10 12:27:07 -0500

    Updating the example to use MAV and TV rather than previous notation.   Also adding an East workbook for example 3

--- inst/Examples/2ArmNormalOutcomeAnalysis/RCode/AnalyzeUsingMeanLimitsOfCI.R
+++ inst/Examples/2ArmNormalOutcomeAnalysis/RCode/AnalyzeUsingMeanLimitsOfCI.R
@@ -6,35 +6,37 @@
 #' @param LookInfo List containing Design and Simulation Parameters, which might be required to perform analysis.
 #' @param UserParam A list of user defined parameters in East or Solara. UserParam must be supplied, the list must contain the following named elements:
 #' \describe{
-#'   \item{UserParam$dLowerLimit}{A value (-Inf, Inf) that specifics the lower limit, eg  Minimum Acceptable Value (MAV). }
-#'   \item{UserParam$dUpperLimit}{A value (-Inf, Inf) that specifies the upper limit for the confidence interval, eg Target Value (TV).}
+#'   \item{UserParam$dMAV}{A value (-Inf, Inf) that specifics the Minimum Acceptable Value (MAV). }
+#'   \item{UserParam$dTV}{A value (-Inf, Inf) that specifies the Target Value (TV).}
 #'   \item{UserParam$dConfLevel}{A value (0,1) that specifies the confidence level for the t.test() function in base R library.}
 #' }
-#' @description  In this simplified example of upper and lower confidence boundary designs, if it is likely that the treatment difference is above the Minimum Acceptable Value (MAV) then a Go decision is made.  
+#' @description  In this simplified example of upper and lower confidence boundary designs, if it is likely that the treatment difference is above 
+#'               the Minimum Acceptable Value (MAV) then a Go decision is made.  
 #'               If a Go decision is not made, then if is is unlikely that the treatment difference is above the Target Value (TV) a No Go decision is made.      
-#'               In this example, the t.test() from base package in R is utilized to analyze the data and compute at user-specified confidence interval (dConfLevel).  
+#'               In this example, the t.test() from base package in R is utilized to analyze the data and compute at user-specified confidence interval using UserParam$dConfLevel.  
 #'               The team would like to make a Go decision if there is at least a 90% chance that the difference in treatment is greater than the MAV.  
 #'               If a Go decision is not made, then a No Go decision is made if there is less than a 10% chance the difference is greater than the TV.  
 #'               Using a frequentist CI an approximation to this design can be done by the logic described below.
-#'               At an analysis, if the Lower Limit of the CI, denoted by LL, is greater than user-specified dLowerLimit then a Go decision is made.  
+#'               At an analysis, if the Lower Limit of the CI, denoted by LL, is greater than user-specified UserParam$dMAV then a Go decision is made.  
 #'               
-#'               If a Go decision is not made, then if the Upper Limit of the CI, denoted by UL, is less than user-specified dUpperLimit a No Go decision is made.  
+#'               If a Go decision is not made, then if the Upper Limit of the CI, denoted by UL, is less than user-specified UserParam$TV a No Go decision is made.  
 #'               Specifically, 
-#'                  if LL > UserParam$dLowerLimit --> Go
-#'                  if UL < UserParam$dUpperLimit --> No Go
+#'                  if LL > UserParam$dMAV --> Go
+#'                  if UL < UserParam$dTV --> No Go
 #'               Otherwise, continue to the next analysis. 
-#'               At the Final Analysis: If LL > UserParam$dLowerLimit  then a Go decision is made, otherwise, a No Go decision is made
+#'               At the Final Analysis: If LL > UserParam$dMAV  then a Go decision is made, otherwise, a No Go decision is made
 #' @return TestStat A double value of the computed test statistic
 #' @return Decision An integer value: Decision = 0 --> No boundary crossed
 #'                                    Decision = 1 --> Lower Efficacy Boundary Crossed
 #'                                    Decision = 2 --> Upper Efficacy Boundary Crossed
 #'                                    Decision = 3 --> Futility Boundary Crossed
 #'                                    Decision = 4 --> Equivalence Boundary Crossed
-#' @return Delta The difference in the estimates, is utilzied in Solara to create the observed graph
+#' @return Delta The difference in the estimates, is utilized in Solara to create the observed graph
 #' @return ErrorCode An integer value:  ErrorCode = 0 --> No Error
 #                                       ErrorCode > 0 --> Non fatal error, current simulation is aborted but the next simulations will run
 #                                       ErrorCode < 0 --> Fatal error, no further simulation will be attempted
-#'@note In this example, the boundary information that is computed and sent from East is ignored in order to implement this decision approach.
+#'@note This function is only applicable to the case where MAV <= TV.  
+#'       In this example, the boundary information that is computed and sent from East is ignored in order to implement this decision approach.
 #'@note Helpful Hints:
 #'       There is often info that East sends to R that are not shown in a given example.  It can be very helpful to save the input 
 #'       objects and then load them into your R session and inspect them.  This can be done with the following R code in your function.
@@ -48,15 +50,8 @@
 ################################################################################################################################################################################################
 AnalyzeUsingMeanLimitsOfCI <- function(SimData, DesignParam, LookInfo = NULL, UserParam = NULL)
 {
-    # Input objects can be saved through the following lines:
-    
-    # setwd( ["Path where we want .Rds files from East to be saved"])
-    # saveRDS( SimData, "SimData.Rds")
-    # saveRDS( DesignParam, "DesignParam.Rds" )
-    # saveRDS( LookInfo, "LookInfo.Rds" )
- 
-    
-    # Retrieve necessary information from the objects East sent
+
+    # Step 1 - Retrieve necessary information from the objects Cytel objects sent ####
     if(  !is.null( LookInfo )  )
     {
         # Group sequential design
@@ -64,9 +59,11 @@ AnalyzeUsingMeanLimitsOfCI <- function(SimData, DesignParam, LookInfo = NULL, Us
         nQtyOfLooks          <- LookInfo$NumLooks
         nQtyOfEvents         <- LookInfo$CumEvents[ nLookIndex ]
         nQtyOfPatsInAnalysis <- LookInfo$CumCompleters[ nLookIndex ]
-    }else
+    }
+    else
     {
         # Fixed Design
+        nLookIndex           <- 1
         nQtyOfLooks          <- 1
         nQtyOfEvents         <- DesignParam$MaxCompleters
         nQtyOfPatsInAnalysis <- nrow( SimData )
@@ -102,7 +99,7 @@ AnalyzeUsingMeanLimitsOfCI <- function(SimData, DesignParam, LookInfo = NULL, Us
     dLowerLimitCI        <- lAnalysisResult$conf.int[ 1 ]
     
     # A decision of 2 means success, 0 means continue the trial
-    nDecision            <- ifelse( dLowerLimitCI > UserParam$dLowerLimit, 2, 0 )  
+    nDecision            <- ifelse( dLowerLimitCI > UserParam$dMAV, 2, 0 )  
     
     if( nDecision == 0 )
     {
@@ -115,14 +112,15 @@ AnalyzeUsingMeanLimitsOfCI <- function(SimData, DesignParam, LookInfo = NULL, Us
         {
             # The final analysis was reached and a Go decision could not be made, thus a No Go decision is made
             nDecision <- 3                                    # East code for futility 
-        }else if( dUpperLimitCI < UserParam$dUpperLimit )     # At the IA check the No Go since a Go decision was not made
+        }
+        else if( dUpperLimitCI < UserParam$dTV ) 
+        {   # At the IA check the No Go since a Go decision was not made
             nDecision <- 3 # East code for futility 
+        }
     }
     
     Error 	<- 0
-    
-    
-    
+
     return( list( TestStat  = as.double( dLowerLimitCI ), 
                 ErrorCode = as.integer( Error ), 
                 Decision  = as.integer( nDecision ),
