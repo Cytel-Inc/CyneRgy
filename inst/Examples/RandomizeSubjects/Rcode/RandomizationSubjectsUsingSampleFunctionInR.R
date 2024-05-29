5df961f3162af04bb6b8c317cc7e4c75cb500c89
Shubham Lahoti <166011767+slahoti17@users.noreply.github.com>
2024-05-29 11:28:09 +0530

    Update RandomizationSubjectsUsingSampleFunctionInR.R

--- inst/Examples/RandomizeSubjects/Rcode/RandomizationSubjectsUsingSampleFunctionInR.R
+++ inst/Examples/RandomizeSubjects/Rcode/RandomizationSubjectsUsingSampleFunctionInR.R
@@ -1,4 +1,3 @@
-
 # Function Template for Randomizing Subjects to Treatments.
 #'@name RandomizationSubjects
 #'@author Shubham Lahoti
@@ -8,6 +7,10 @@
 #' 1) Let p = Allocation fraction on Control arm and 1 - p = Allocation fraction on treatment arm.
 #' 2) Compute Expected Sample size (rounded) for Control and treatment arms using Allocation Fraction and Total sample size.
 #' 3) Generate a Binary vector where nC = Control sample size and nT = Treatment sample size using sample() functionality available in R.
+#' 
+#' @param NumSub: The number of subjects that need to be simulated, integer value
+#' @param NumArm: The number of arms in the trial including experimental and control, integer value
+#' @param AllocRatio: Tthe ratio of the experimental group sample size (nt) to control group sample size (nc) i.e. (nt/nc).
 #' 
 #' @return ErrorCode An integer value:  ErrorCode = 0 --> No Error
 #                                       ErrorCode > 0 --> Non fatal error, current simulation is aborted but the next simulations will run
@@ -19,26 +22,23 @@
 RandomizationSubjectsUsingSampleFunctionInR <- function(NumSub, NumArms, AllocRatio, UserParam = NULL)
 {
     
-    Error 	= 0
-    
-    dNumSub                       <- NumSub            # Total Sample size
-    dNumArms                      <- 2                 # two arm designs
-    
+    Error 	                      <- 0
+
     # Allocation ratio on control and treatment arm
     vAllocRatio                   <- c( 1, AllocRatio )
     
     # Convert the Allocation Ratio to Allocation Fraction for control and treatment arm
     dAllocFraction                <- c( vAllocRatio[ 1 ]/sum( vAllocRatio ), 1 - vAllocRatio[ 1 ]/sum( vAllocRatio ) )
-    vSampleSizeArmWise            <- c( round( dNumSub * dAllocFraction[ 1 ]), dNumSub - round( dNumSub * dAllocFraction[ 1 ] ) )
+    vSampleSizeArmWise            <- c( round( NumSub * dAllocFraction[ 1 ]), NumSub - round( NumSub * dAllocFraction[ 1 ] ) )
     
     # Find the indices for Control and treatment arms
-    vControlArmIndex              <- sample( 1:dNumSub, size = vSampleSizeArmWise[ 1 ], replace = FALSE )
-    vTreatmentArmIndex            <- c( 1:dNumSub )[ -vControlArmIndex ]
+    vControlArmIndex              <- sample( 1:NumSub, size = vSampleSizeArmWise[ 1 ], replace = FALSE )
+    vTreatmentArmIndex            <- c( 1:NumSub )[ -vControlArmIndex ]
     
     # Generate a vector of zeroes of size NumSub and then replace the Treatment Indices with 1.
     
     retval                        <- rep( 0, NumSub )
-    retval[ vTreatmentArmIndex ]  =  1
+    retval[ vTreatmentArmIndex ]  <-  1
     
     return(list(TreatmentID = as.integer(retval), ErrorCode = as.integer(Error)))
 }
