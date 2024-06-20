87858cdda9119a8c3de44393ed2e25d51da3812b
Audrey Wathen <AudreyWathen517@gmail.com>
2024-06-20 09:44:09 -0500

    test addition to AnalyzeUsingTTestNormal

--- tests/testthat/test-AnalyzeUsingTTestNormal.R
+++ tests/testthat/test-AnalyzeUsingTTestNormal.R
@@ -7,13 +7,63 @@
 #################################################################################################### .
 
 
-context( "AnalyzeUsingPropTest")
+context( "AnalyzeUsingTTestNormal")
 
-test_that("Test- AnalyzeUsingPropTest", {
-    # Example test that will fail
-    # nRet         <- 1
-    # nExpectedRet <- 10
+test_that("Test- AnalyzeUsingTTestNormal", {
+    nQtyOfPatientsPerArm <- 125
+    nQtyOfPatients       <- 2*nQtyOfPatientsPerArm
     
+    vTreatmentID        <- c( rep(0,nQtyOfPatientsPerArm ), rep( 1, nQtyOfPatientsPerArm) )
+    vPatientResponseStd <- rbinom( nQtyOfPatientsPerArm,1, 0.25 )
+    vPatientResponseExp <- rbinom( nQtyOfPatientsPerArm,1, 0.5 )
+    vPatientResponse    <- c( vPatientResponseStd, vPatientResponseExp )
     
-    # expect_equal( nRet, nExpectedRet, info = "The test failed...", label ="Test for ..." )
+    vRandomIndex        <- sample( 1:nQtyOfPatients,  size = nQtyOfPatients )
+    
+    vPatientResponse    <- vPatientResponse[ vRandomIndex ]
+    vTreatmentID        <- vTreatmentID[ vRandomIndex ]
+    
+    SimData     <- list( TreatmentID = vTreatmentID, Response = vPatientResponse)
+    LookInfo    <- list( NumLooks = 3, CurrLooKIndex = 1, CumCompleters = c( nQtyOfPatients/2, nQtyOfPatients ))
+    DesignParam <- list( SampleSize = nQtyOfPatients, MaxCompleters = nQtyOfPatients)
+    
+    #Test 1  LookInfo = NULL, UserParam = NULL ####
+    
+    lRet1 <- tryCatch({
+        AnalyzeUsingTTestNormal( SimData, DesignParam, LookInfo = NULL, UserParam = NULL )
+    }, error = function(e) {
+        NULL
+    })
+    
+    lExpRet1 <- list( TestStat = NULL, ErrorCode = 0, Decision = 0, Delta = 0 ) 
+    
+    expect_equal( lRet1, lExpRet1, info = "Test 1: Return list did not match")
+    
+    #Test 2  UserParam = NULL####
+    
+    lRet2 <- tryCatch({
+        AnalyzeUsingTTestNormal( SimData, DesignParam, LookInfo = LookInfo, UserParam = NULL )
+    }, error = function(e) {
+        NULL
+    })
+    
+    lExpRet2 <- list( TestStat = NULL, ErrorCode = 0, Decision = 0, Delta = 0 ) 
+    
+    expect_equal( lRet2, lExpRet2, info = "Test 2: Return list did not match")
+    
+    #Test 3 omit LookInfo, UserParam ####
+    
+    lRet3 <- tryCatch({
+        AnalyzeUsingTTestNormal( SimData, DesignParam )
+    }, error = function(e) {
+        NULL
+    })
+    
+    lExpRet3 <- list( TestStat = NULL, ErrorCode = 0, Decision = 0, Delta = 0 ) 
+    
+    expect_equal( lRet3, lExpRet3, info = "Test 3: Return list did not match")
+    
 })
+
+
+
