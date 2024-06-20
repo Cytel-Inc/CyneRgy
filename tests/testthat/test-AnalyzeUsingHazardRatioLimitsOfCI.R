e1d20797d0b2b5e88661c71e0482ab6b0aafffb3
Audrey Wathen <AudreyWathen517@gmail.com>
2024-06-20 09:12:26 -0500

    adding test for additional analyze functions

--- tests/testthat/test-AnalyzeUsingHazardRatioLimitsOfCI.R
+++ tests/testthat/test-AnalyzeUsingHazardRatioLimitsOfCI.R
@@ -1,7 +1,7 @@
 #################################################################################################### .
 #   Program/Function Name:
-#   Author:Audrey Wathen
-#   Description: Test file for AnalyzeUsingPropTest
+#   Author: Audrey Wathen
+#   Description: Test file for 
 #   Change History:
 #   Last Modified Date: 06/20/2024
 #################################################################################################### .
@@ -13,7 +13,7 @@ test_that("Test- AnalyzeUsingPropTest", {
     # Example test that will fail
     # nRet         <- 1
     # nExpectedRet <- 10
-
-
+    
+    
     # expect_equal( nRet, nExpectedRet, info = "The test failed...", label ="Test for ..." )
 })
