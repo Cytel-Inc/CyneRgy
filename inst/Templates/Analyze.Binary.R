d8eb961aa2807e92f7fa5c49163684b83ea2dba3
Anoop Rawat <Anoop.Rawat@cytel.com>
2024-10-10 12:47:04 +0530

    Updated the analysis templates to use GetDecision

--- inst/Templates/Analyze.Binary.R
+++ inst/Templates/Analyze.Binary.R
@@ -77,12 +77,16 @@
 #'                      }
 {{FUNCTION_NAME}} <- function(SimData, DesignParam, LookInfo = NULL, UserParam = NULL )
 {
-    nError 	        <- 0
-    nDecision 	    <- 0
-    dTestStatistic  <- 0
+    library(CyneRgy)
+    
+    nError 	         <- 0
+    nDecision 	     <- 0
+    dTestStatistic   <- 0
+    bIAEfficayCheck  <- TRUE
+    bIAFutilityCheck <- FALSE
+    bFAEfficacyCheck <- TRUE
     
     # Step 1 - If LookInfo is Null, then this is a fixed design and we use the DesignParam$MaxEvents
-    nLookIndex           <- 1 
     if(  !is.null( LookInfo )  )
     {
         nQtyOfLooks          <- LookInfo$NumLooks
@@ -92,10 +96,40 @@
     else
     {
         nQtyOfLooks          <- 1
+        nLookIndex           <- 1
         nQtyOfPatsInAnalysis <- nrow( SimData )
     }
 
+    # Setup look decision logic
+    if( nLookIndex < nQtyOfLooks )  # Interim Analysis
+    {
+        
+        if( bIAEfficayCheck )
+        {
+            strDecision <- "Efficacy"
+        }
+        else if( bIAFutilityCheck )
+        {
+            strDecision <- "Futility"
+        }
+        else
+        {
+            strDecision <- "Continue"
+        }
+    }
+    else # Final Analysis
+    {
+        if( bFAEfficacyCheck  )
+        {
+            strDecision <- "Efficacy"
+        }
+        else
+        {
+            strDecision <- "Futility"
+        }
+    }
     
+    nDecision <- CyneRgy::GetDecision( strDecision, DesignParam, LookInfo )
 
     lRet <- list(TestStat = as.double(dTestStatistic),
                  Decision  = as.integer(nDecision), 
